ef9d1201746c3550b35072fe7c4fac6adb2a2fcf
Sandeep <sandeepnegi686@gmail.com>
2026-01-23 11:55:19 +0530

    changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // enables static export
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
